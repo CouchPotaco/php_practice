1e5abbd78d9101fdaf096463b0bfad3c3eee96a3
m.shimamura <m.shimamura@ae.auone-net.jp>
2022-09-28 02:59:44 +0000

    php05更新

--- task-php05.php
+++ task-php05.php
@@ -13,10 +13,11 @@ if ($myName == "島村") {
 
 <?php
 // 2.for文を使って、1から10000までの合計の値を表示してください
+$total=0;
 for ($i=1; $i<=10000; $i++) {
-	echo $i;
-	echo "\n";
+	$total += $i;
 }
+	echo $total;
 ?>
 <br/>
 
